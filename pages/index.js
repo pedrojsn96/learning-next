60187853225d6340b2962359cd5a84545ce9bfb9
Pedro Neto <pedro.neto@ifood.com.br>
2021-01-26 13:57:57 -0300

    first step with next

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,61 @@
-import styled from 'styled-components'
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import React from 'react';
+import Widget from '../src/components/Widget';
+import db from '../db.json';
+import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <title>AluraQuiz - Modelo Base</title>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={() => {}}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit">
+                Jogar
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/pedrojsn96" />
+    </QuizBackground>
+  );
 }
